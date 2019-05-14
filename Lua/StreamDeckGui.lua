04fc73f2b8ef483fb5c12efc5ac28a4244506f44
CJ Garcia <CJGarciaMusic@gmail.com>
2019-05-14 11:45:41 -0600

    Changed the Lua gui to be a list, added an applescript that selects from that Lua list

--- Lua/StreamDeckGui.lua
+++ Lua/StreamDeckGui.lua
@@ -4,7 +4,7 @@ function plugindef()
     finaleplugin.Copyright = "© 2019 CJ Garcia Music"
     finaleplugin.Version = "0.1"
     finaleplugin.Date = "5/14/2019"
-    return "Stream Deck", "Stream Deck", "Stream Deck commands and gui"
+    return "Stream Deck", "Stream Deck", "A selectable list of all Lua functions related to Stream Deck"
 end
 
 local str = finale.FCString()
@@ -12,135 +12,150 @@ str.LuaString = "Stream Deck for Finale"
 local dialog = finale.FCCustomLuaWindow()
 dialog:SetTitle(str)
 
-local button_width = 90
+local static_text = dialog:CreateStatic(0, 0)
+str.LuaString = "Select Your Action"
+static_text:SetWidth(200)
+static_text:SetText(str)
 
-local button_1 = dialog:CreateButton(0, 0)
+local listbox = dialog:CreateListBox(0, 20)
+listbox:SetWidth(200)
 str.LuaString = "Function 1"
-button_1:SetText(str)
-button_1:SetWidth(button_width)
+listbox:AddString(str)
+local function func_1()
+    finenv.UI():AlertInfo("you pressed button 1, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_2 = dialog:CreateButton(100, 0)
 str.LuaString = "Function 2"
-button_2:SetText(str)
-button_2:SetWidth(button_width)
+listbox:AddString(str)
+local function func_2()
+    finenv.UI():AlertInfo("you pressed button 2, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_3 = dialog:CreateButton(200, 0)
 str.LuaString = "Function 3"
-button_3:SetText(str)
-button_3:SetWidth(button_width)
+listbox:AddString(str)
+local function func_3()
+    finenv.UI():AlertInfo("you pressed button 3, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_4 = dialog:CreateButton(300, 0)
 str.LuaString = "Function 4"
-button_4:SetText(str)
-button_4:SetWidth(button_width)
+listbox:AddString(str)
+local function func_4()
+    finenv.UI():AlertInfo("you pressed button 4, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_5 = dialog:CreateButton(400, 0)
 str.LuaString = "Function 5"
-button_5:SetText(str)
-button_5:SetWidth(button_width)
+listbox:AddString(str)
+local function func_5()
+    finenv.UI():AlertInfo("you pressed button 5, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_6 = dialog:CreateButton(0, 50)
 str.LuaString = "Function 6"
-button_6:SetText(str)
-button_6:SetWidth(button_width)
+listbox:AddString(str)
+local function func_6()
+    finenv.UI():AlertInfo("you pressed button 6, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_7 = dialog:CreateButton(100, 50)
 str.LuaString = "Function 7"
-button_7:SetText(str)
-button_7:SetWidth(button_width)
+listbox:AddString(str)
+local function func_7()
+    finenv.UI():AlertInfo("you pressed button 7, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_8 = dialog:CreateButton(200, 50)
 str.LuaString = "Function 8"
-button_8:SetText(str)
-button_8:SetWidth(button_width)
+listbox:AddString(str)
+local function func_8()
+    finenv.UI():AlertInfo("you pressed button 8, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_9 = dialog:CreateButton(300, 50)
 str.LuaString = "Function 9"
-button_9:SetText(str)
-button_9:SetWidth(button_width)
+listbox:AddString(str)
+local function func_9()
+    finenv.UI():AlertInfo("you pressed button 9, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_10 = dialog:CreateButton(400, 50)
 str.LuaString = "Function 10"
-button_10:SetText(str)
-button_10:SetWidth(button_width)
+listbox:AddString(str)
+local function func_10()
+    finenv.UI():AlertInfo("you pressed button 10, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_11 = dialog:CreateButton(0, 100)
 str.LuaString = "Function 11"
-button_11:SetText(str)
-button_11:SetWidth(button_width)
+listbox:AddString(str)
+local function func_11()
+    finenv.UI():AlertInfo("you pressed button 11, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-
-local button_12 = dialog:CreateButton(100, 100)
 str.LuaString = "Function 12"
-button_12:SetText(str)
-button_12:SetWidth(button_width)
+listbox:AddString(str)
+local function func_12()
+    finenv.UI():AlertInfo("you pressed button 12, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_13 = dialog:CreateButton(200, 100)
 str.LuaString = "Function 13"
-button_13:SetText(str)
-button_13:SetWidth(button_width)
+listbox:AddString(str)
+local function func_13()
+    finenv.UI():AlertInfo("you pressed button 13, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_14 = dialog:CreateButton(300, 100)
 str.LuaString = "Function 14"
-button_14:SetText(str)
-button_14:SetWidth(button_width)
+listbox:AddString(str)
+local function func_14()
+    finenv.UI():AlertInfo("you pressed button 14, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
-local button_15 = dialog:CreateButton(400, 100)
 str.LuaString = "Function 15"
-button_15:SetText(str)
-button_15:SetWidth(button_width)
+listbox:AddString(str)
+local function func_15()
+    finenv.UI():AlertInfo("you pressed button 15, nothing is going to happen though #DealWithIt",  "Sucks to suck.") 
+end
 
 dialog:CreateOkButton()
 dialog:CreateCancelButton()
 
-local function mycommandhandler(thecontrol)
-    if thecontrol:GetControlID() == button_1:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 1, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_2:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 2, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_3:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 3, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_4:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 4, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_5:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 5, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_6:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 6, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_7:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 7, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_8:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 8, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_9:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 9, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_10:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 10, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_11:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 11, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_12:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 12, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_13:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 13, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_14:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 14, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-    if thecontrol:GetControlID() == button_15:GetControlID() then
-        finenv.UI():AlertInfo("you pressed button 15, nothing is going to happen though #DealWithIt",  "Sucks to suck.")
-    end
-end
-
-dialog:RegisterHandleCommand(mycommandhandler)
 if (dialog:ExecuteModal(nil) == 1) then
+    if listbox:GetSelectedItem() == 0 then
+        func_1()
+    end
+    if listbox:GetSelectedItem() == 1 then
+        func_2()
+    end
+    if listbox:GetSelectedItem() == 2 then
+        func_3()
+    end
+    if listbox:GetSelectedItem() == 3 then
+        func_4()
+    end
+    if listbox:GetSelectedItem() == 4 then
+        func_5()
+    end
+    if listbox:GetSelectedItem() == 5 then
+        func_6()
+    end
+    if listbox:GetSelectedItem() == 6 then
+        func_7()
+    end
+    if listbox:GetSelectedItem() == 7 then
+        func_8()
+    end
+    if listbox:GetSelectedItem() == 8 then
+        func_9()
+    end
+    if listbox:GetSelectedItem() == 9 then
+        func_10()
+    end
+    if listbox:GetSelectedItem() == 10 then
+        func_11()
+    end
+    if listbox:GetSelectedItem() == 11 then
+        func_12()
+    end
+    if listbox:GetSelectedItem() == 12 then
+        func_13()
+    end
+    if listbox:GetSelectedItem() == 13 then
+        func_14()
+    end
+    if listbox:GetSelectedItem() == 14 then
+        func_15()
+    end
 end
--- Mac/AppleScript/Templates/Lua_Access.applescript
+++ Mac/AppleScript/Templates/Lua_Access.applescript
@@ -1,4 +1,4 @@
-on subMenuItem(theMenuName, theMenuItemName, theSubMenuItem)
+on subMenuItem(theMenuName, theMenuItemName, theSubMenuItem, windowName, listItem)
 	tell application "System Events"
 		set appName to name of the first process whose frontmost is true
 	end tell
@@ -6,15 +6,19 @@ on subMenuItem(theMenuName, theMenuItemName, theSubMenuItem)
 		tell application "System Events"
 			tell process appName
 				click menu item theSubMenuItem of menu of menu item theMenuItemName of menu theMenuName of menu bar 1
+                tell table 1 of scroll area 1 of window windowName
+                    select (row 1 where value of text field 1 is listItem)
+                end tell
+                click button "OK" of window windowName
 			end tell
 		end tell
 		return true
 	on error
 		set theAlertText to "A Stream Deck error has occurred."
-        set theAlertMessage to "The " & theSubMenuItem & " tool wasn't able to be selected.\n\nPlease try again."
+        set theAlertMessage to listItem & "  wasn't able to be selected.\n\nPlease try again."
         display alert theAlertText message theAlertMessage as critical
 		return false
 	end try
 end subMenuItem
 
-subMenuItem("Tools", "Simple Entry", "16th note")
+subMenuItem("Plug-ins", "JW Lua", "Stream Deck", "Stream Deck for Finale", "Function 15")
