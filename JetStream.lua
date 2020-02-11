21689f49708b89ebca29988757cfeb17a2a73d8c
CJ Garcia <CJGarciaMusic@gmail.com>
2020-02-11 15:07:20 -0700

    Now should be fixed

--- JetStream.lua
+++ JetStream.lua
@@ -663,7 +663,7 @@ end
 function setFirstLastNoteRangeEntry(smart_shape)
     local music_region = finenv.Region()
     local range_settings = {}
-    
+    music_region:SetCurrentSelection()
     for addstaff = music_region:GetStartStaff(), music_region:GetEndStaff() do
         music_region:SetStartStaff(addstaff)
         music_region:SetEndStaff(addstaff)
