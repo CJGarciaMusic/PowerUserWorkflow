0d7520ef67bc9ba9508492c96f8413c126710b21
CJ-G <cgarcia@makemusic.com>
2020-10-11 16:26:14 -0600

    updated the version date

--- JetStream.lua
+++ JetStream.lua
@@ -1,7 +1,7 @@
 function plugindef()
     finaleplugin.RequireSelection = false
-    finaleplugin.Version = "200630"
-    finaleplugin.Date = "6/30/2020"
+    finaleplugin.Version = "201011"
+    finaleplugin.Date = "10/11/2020"
     return "JetStream Finale Controller", "JetStream Finale Controller", "Input four digit codes to access JetStream Finale Controller features."
 end
 
